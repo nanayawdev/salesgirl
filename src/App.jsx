bfc42bcb4ab80dcde200cfc8568e9dc45448eff0
nanayawdev <nanayawisrael@gmail.com>
2024-12-01 23:38:44 +0000

    Add InvoiceGenerator component to App for enhanced invoice creation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import InvoiceFeatures from './components/InvoiceFeatures/InvoiceFeatures';
 import InvoiceSteps from './components/InvoiceSteps/InvoiceSteps';
+import InvoiceGenerator from './components/InvoiceGenerator/InvoiceGenerator';
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
       <HeroSection />
       <InvoiceFeatures />
       <InvoiceSteps />
+      <InvoiceGenerator />
     </>
   );
 }
