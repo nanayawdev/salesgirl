024a5b1baed30983c0461d79bf82a3875ead7ddf
nanayawdev <nanayawisrael@gmail.com>
2024-12-01 20:49:50 +0000

    Update App component in App.jsx to include Navbar component and remove "Hello World" heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello World</h1>
+      <Navbar />
     </>
   )
 }
