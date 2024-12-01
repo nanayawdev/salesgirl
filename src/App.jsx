1f7801ad9e945a1ff733e3cd82c48eed428b53fe
nanayawdev <nanayawisrael@gmail.com>
2024-12-01 23:05:11 +0000

    Add InvoiceSteps component to App for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import InvoiceFeatures from './components/InvoiceFeatures/InvoiceFeatures';
+import InvoiceSteps from './components/InvoiceSteps/InvoiceSteps';
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
       <InvoiceFeatures />
+      <InvoiceSteps />
     </>
   );
 }
