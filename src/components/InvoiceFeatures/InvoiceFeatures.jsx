da3382ce392bece85917d68ba38bd8bed0d2f2b5
nanayawdev <nanayawisrael@gmail.com>
2024-12-03 13:45:45 +0000

    Refactor InvoiceFeatures component for improved styling and visual consistency
    - Updated background and hover styles in the InvoiceFeatures component to enhance design coherence.
    - Changed color classes for better alignment with the overall design language, ensuring a more cohesive user experience across themes.

--- src/components/InvoiceFeatures/InvoiceFeatures.jsx
+++ src/components/InvoiceFeatures/InvoiceFeatures.jsx
@@ -15,9 +15,9 @@ const InvoiceFeatures = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8">
-          <div className="group p-4 sm:p-6 rounded-2xl bg-codGray-50 dark:bg-background-dark transition-all duration-300 border border-codGray-100 dark:border-codGray-400 hover:border-emerald-100 dark:hover:border-emerald-600 hover:-translate-y-1">
+          <div className="group p-4 sm:p-6 rounded-2xl bg-codGray-50 dark:bg-codGray-800 transition-all duration-300 border border-codGray-100 dark:border-codGray-400 hover:border-algaeGreen-100 dark:hover:border-algaeGreen-600 hover:-translate-y-1">
             <div className="flex items-center gap-3 mb-2 sm:mb-3">
-              <div className="p-3 rounded-xl bg-emerald-50 dark:bg-gray-700 w-fit group-hover:bg-emerald-100 dark:group-hover:bg-gray-600/30 transition-colors duration-300">
+              <div className="p-3 rounded-xl bg-algaeGreen-50 dark:bg-gray-700 w-fit group-hover:bg-algaeGreen-100 dark:group-hover:bg-gray-600/30 transition-colors duration-300">
                 <DocumentTextIcon className="icon-base" />
               </div>
               <h3 className="text-xl phone:text-base tablet:text-lg laptop:text-xl desktop:text-xl font-semibold text-codGray-900 dark:text-codGray-100 group-hover:text-emerald-600 dark:group-hover:text-emerald-600 transition-colors duration-300">
