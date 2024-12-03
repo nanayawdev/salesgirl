b91243669df58d8464793708d35aa4edbe0de950
nanayawdev <nanayawisrael@gmail.com>
2024-12-03 12:25:06 +0000

    Refactor NoticeBar component styling for improved readability and consistency
    - Updated text colors in the NoticeBar component to enhance contrast and readability, ensuring better visibility against the background.
    - Adjusted hover states to maintain visual consistency with the overall design theme.
    - These changes contribute to a more cohesive user interface across the application.

--- src/components/ui/noticebar.jsx
+++ src/components/ui/noticebar.jsx
@@ -4,13 +4,13 @@ const NoticeBar = () => {
   return (
     <a
       href="/status"
-      className="hidden tablet:flex items-center rounded-full bg-algaeGreen-400 dark:bg-algaeGreen-500 px-3 py-1 hover:bg-algaeGreen-600 dark:hover:bg-algaeGreen-300 transition-colors cursor-pointer"
+      className="hidden tablet:flex items-center rounded-full bg-algaeGreen-400 dark:bg-algaeGreen-400 px-3 py-1 hover:bg-algaeGreen-300 dark:hover:bg-algaeGreen-300 transition-colors cursor-pointer"
     >
       <span className="font-medium text-xs text-codGray-900 dark:text-codGray-800 whitespace-nowrap">
         Introducing Status
       </span>
       <span className="mx-2 text-codGray-900">·</span>
-      <span className="text-xs text-codGray-600 dark:text-codGray-400 whitespace-nowrap">
+      <span className="text-xs text-codGray-900 dark:text-codGray-900 whitespace-nowrap">
         Our new status reporting
       </span>
       <svg 
