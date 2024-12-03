6cd62abac30de42f4f7a6b2c45949fc6775ace8d
nanayawdev <nanayawisrael@gmail.com>
2024-12-03 12:11:51 +0000

    Update NoticeBar component styling for improved visual consistency
    - Changed background color to a darker 'algaeGreen' for better contrast and updated text colors to 'codGray' for enhanced readability.
    - These adjustments align the NoticeBar with the overall design updates and improve the user interface aesthetics.

--- src/components/ui/noticebar.jsx
+++ src/components/ui/noticebar.jsx
@@ -4,13 +4,13 @@ const NoticeBar = () => {
   return (
     <a
       href="/status"
-      className="hidden tablet:flex items-center rounded-full bg-algaeGreen-50 dark:bg-algaeGreen-300 px-3 py-1 hover:bg-algaeGreen-100 dark:hover:bg-algaeGreen-700 transition-colors cursor-pointer"
+      className="hidden tablet:flex items-center rounded-full bg-algaeGreen-400 dark:bg-algaeGreen-300 px-3 py-1 hover:bg-algaeGreen-600 dark:hover:bg-algaeGreen-700 transition-colors cursor-pointer"
     >
-      <span className="font-medium text-xs text-gray-900 dark:text-white whitespace-nowrap">
+      <span className="font-medium text-xs text-codGray-900 dark:text-white whitespace-nowrap">
         Introducing Status
       </span>
-      <span className="mx-2 text-gray-400">·</span>
-      <span className="text-xs text-gray-600 dark:text-gray-400 whitespace-nowrap">
+      <span className="mx-2 text-codGray-900">·</span>
+      <span className="text-xs text-codGray-600 dark:text-codGray-400 whitespace-nowrap">
         Our new status reporting
       </span>
       <svg 
