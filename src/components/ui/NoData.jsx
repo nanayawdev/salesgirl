2fb370170c6c3937e27808958993081e1acfc133
nanayawdev <nanayawisrael@gmail.com>
2024-12-05 15:49:11 +0000

    Remove PlusCircleIcon from NoData component for a cleaner design; retains message display and link functionality.

--- src/components/ui/NoData.jsx
+++ src/components/ui/NoData.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { PlusCircleIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 
 const NoData = ({ message, createLink, createText }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[200px]">
-      <PlusCircleIcon className="w-12 h-12 text-gray-400 mb-4" />
       <p className="text-gray-500 mb-4">{message}</p>
       <Link
         to={createLink}
