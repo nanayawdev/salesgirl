e30c617db2b897135af4e565b47f1a3b6b25e8a2
nanayawdev <nanayawisrael@gmail.com>
2024-12-05 15:24:53 +0000

    Enhance Navbar component by adding z-index to dropdown menu for improved visibility

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 mt-2 w-48 bg-white dark:bg-background-dark rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 mt-2 w-48 bg-white dark:bg-background-dark rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                       <Menu.Item>
                         {({ active }) => (
                           <Link
